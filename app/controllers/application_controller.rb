1570e8cd217ee28de3f0d7b93585332427aaa23b
vaitheeswarivenkatesan <v.vaithees4@gmail.com>
2021-07-02 23:57:05 +0530

    Add ensure_user_loged function in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,42 @@
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
+
+  before_action :ensure_user_logged_in
+
+  def ensure_user_logged_in
+    unless current_user
+      redirect_to "/"
+    else
+      if session[:current_cart_id]
+        cart = Cart.where(id: session[:current_cart_id])
+        if cart.present?
+          @current_cart = cart
+        else
+          session[:current_cart_id] = nil
+        end
+      end
+      if session[:current_cart_id] == nil
+        @current_cart = Cart.create!(user_id: session[:current_user_id])
+        session[:current_cart_id] = @current_cart.id
+        @current_cart.update(total_price: @current_cart.cart_total)
+      end
+      # render plain: "cart id:#{session[:current_cart_id]}, use id:#{@current_user.id}"
+    end
+  end
+
+  def ensure_owner
+    unless @current_user.is_owner?
+      redirect_to(menu_categories_path, alert: "Your'e not allowed to access the page")
+    end
+  end
+
+  def current_user
+    return @current_user if @current_user
+    current_user_id = session[:current_user_id]
+    if current_user_id
+      @current_user = User.find(current_user_id)
+    else
+      nil
+    end
+  end
 end
