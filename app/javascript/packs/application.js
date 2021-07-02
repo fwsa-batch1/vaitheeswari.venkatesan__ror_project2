1561af5a31473a997e812dea8d721a85f8cc9c4f
vaitheeswarivenkatesan <v.vaithees4@gmail.com>
2021-07-02 23:54:09 +0530

    import bootstrap in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'popper.js'
+import "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
+
 
 Rails.start()
 Turbolinks.start()
